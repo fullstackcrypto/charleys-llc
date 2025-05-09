ebd34b90c1e08e0fb438a3f496f33d490c69d63c
fullstackcrypto <cryptosluss@gmail.com>
2025-05-09 16:33:15 -0700

    Fix Heroku R10 port binding

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 @app.route('/')
 def index():
