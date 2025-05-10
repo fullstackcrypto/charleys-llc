e4e623e97aa78fd6fc4e27b71f5b58c74871307e
fullstackcrypto <cryptosluss@gmail.com>
2025-05-09 17:52:05 -0700

    Fix Heroku R10 port binding

--- app.py
+++ app.py
@@ -21,3 +21,8 @@ def thankyou():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
