cc0496c30bf6c4435fec400bfb881d54bed418f5
Char <80181895+fullstackcrypto@users.noreply.github.com>
2025-11-09 18:22:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, template_folder='templates', static_folder='static')
+from flask import Flask, render_template, request, redirect
 import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+app = Flask(**name**, template_folder=‘templates’, static_folder=‘static’)
 
-@app.route('/')
+@app.route(’/’)
 def index():
-    return render_template('index.html')
+return render_template(‘index.html’)
 
-@app.route('/contact')
+@app.route(’/services’)
+def services():
+return render_template(‘services.html’)
+
+@app.route(’/pricing’)
+def pricing():
+return render_template(‘pricing.html’)
+
+@app.route(’/booking’)
+def booking():
+return render_template(‘booking.html’)
+
+@app.route(’/contact’)
 def contact():
-    return render_template('contact.html')
+return render_template(‘contact.html’)
 
-@app.route('/thankyou')
-def thankyou():
-    return render_template('thankyou.html')
+@app.route(’/thanks’)
+def thanks():
+return render_template(‘thanks.html’)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-import os
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+if **name** == ‘**main**’:
+port = int(os.environ.get(“PORT”, 5000))
+app.run(debug=True, host=‘0.0.0.0’, port=port)
